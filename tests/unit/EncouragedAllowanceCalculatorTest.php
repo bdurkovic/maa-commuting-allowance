a4ac20acf40bbafa0e9557aed7306f3abb173c78
Bojan Đurković <djurkovic@gmail.com>
2018-11-11 23:31:06 +0100

    Added test for encouraged allowance calculator

---
+++ tests/unit/EncouragedAllowanceCalculatorTest.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use CommutingAllowance\Transport\TransportationMethodFactory;
+
+class EncouragedAllowanceCalculatorTest extends \Codeception\Test\Unit {
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+    
+    protected function _before() {
+
+    }
+
+    protected function _after() {
+
+    }
+
+    public function testEncouragedAllowanceCalculator() {
+		$appConfiguration = parse_ini_file('../../config.ini', true);
+    	$bike = TransportationMethodFactory::createFromString('Bike', $appConfiguration);
+		$employee = new \CommutingAllowance\Employee('Matthijs', 11, 4.5, $bike);
+		$this->assertEquals(368.0, $employee->getMonthlyAllowance(1, 2018));
+    }
+}
